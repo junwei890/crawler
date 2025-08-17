c7d44c21047a7888e5f9731628fe0c2cdf73a74a
jdub4asdfg <junweilee72@gmail.com>
2025-08-17 17:02:38 +0800

    removing read from home dir and readme

--- main.go
+++ main.go
@@ -3,37 +3,23 @@ package main
 import (
 	"log"
 	"os"
-	"path"
 	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/junwei890/crawler/src"
 )
 
 func main() {
-	homeDir, err := os.UserHomeDir()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	mongoURI := os.Getenv("MONGO_URI")
+
+	linksInBytes, err := os.ReadFile("crawler.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fileToRead := "crawler.txt"
-	path := path.Join(homeDir, fileToRead)
-
-	// #nosec G304
-	fullFile, err := os.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lines := strings.Fields(string(fullFile))
-
-	mongoURI := ""
-	links := []string{}
-	for i, line := range lines {
-		if i == 0 {
-			mongoURI = line
-		} else if i > 0 {
-			links = append(links, strings.TrimSpace(line))
-		}
-	}
+	links := strings.Fields(string(linksInBytes))
 
 	if err := src.StartCrawl(mongoURI, links); err != nil {
 		log.Fatal(err)
